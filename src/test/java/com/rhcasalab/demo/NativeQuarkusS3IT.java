f42d4b35b14f3af0048e431eb0e963c5c1e5e64d
Martin Ouimet <marouim@gmail.com>
2021-03-30 08:38:58 -0400

    first commit

---
+++ src/test/java/com/rhcasalab/demo/NativeQuarkusS3IT.java
@@ -0,0 +1,9 @@
+package com.rhcasalab.demo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeQuarkusS3IT extends QuarkusS3Test {
+
+    // Execute the same tests but in native mode.
+}
