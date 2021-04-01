204e69d6580b67de3e5998a569c78f865cfcaa9a
Martin Ouimet <marouim@gmail.com>
2021-04-01 13:05:13 -0400

    removed tests

--- src/test/java/com/rhcasalab/demo/NativeQuarkusS3IT.java
+++
@@ -1,9 +0,0 @@
-package com.rhcasalab.demo;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeQuarkusS3IT extends QuarkusS3Test {
-
-    // Execute the same tests but in native mode.
-}
